f60f4aebac526993101791463e822495edc80944
Mahtabvariyani <mahtabvariyani@gmail.com>
2024-01-09 12:34:23 +0100

    added static folder

--- server/index.js
+++ server/index.js
@@ -8,15 +8,20 @@ app.use(bodyParser.json());
 app.use(cors());
 
 const posts = require("./routes/api/posts");
-app.use('/api/posts',posts)
+app.use("/api/posts", posts);
+
+//hanlde Production
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(__dirname + "/public/"));
+  app.get(/.*/,(req,res)=> res.sendFile(__dirname + '/public/index.html'))
+}
 
 const port = process.env.PORT | 5000;
 
-
-app.get('/',(req,res)=>{
-    res.send('Hello')
-})
-
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
 
 app.listen(port, () => {
   console.log(`server started on port ${port}`);
